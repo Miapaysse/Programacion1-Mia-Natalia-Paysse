717b84cddfedf95b312889256bab2295392ec122
Miapaysse <mia.natalia.paysse@gmail.com>
2026-06-02 18:50:09 -0400

    funcion modificada para sumar el valor de los digitos

--- TP11/e3/main.c
+++ TP11/e3/main.c
@@ -4,9 +4,16 @@ int suma_digitos(int n1);
 
 int main (void){
 
+printf("%d\n", suma_digitos(10));
+
 return 0;
 }
 
 int suma_digitos(int n1){
- return ((n1 % 10) + digitos(n1 / 10)); //caso recursivo
+	if(n1/10==0){
+		return n1%10;
+	}
+	else{	
+ 		return ((n1 % 10) + suma_digitos(n1 / 10)); //caso recursivo
+ 	}
 }
