c4b0d874b3ae71ebdef611412fe8c4ede02ff9a4
Miapaysse <mia.natalia.paysse@gmail.com>
2026-06-06 11:17:17 -0400

    Esta funcion hace lo pedido pero en el main no podemos tratar a la matriz como una matriz, sino como un arreglo de n*n doubles

--- TP13/e1/e1.c
+++ TP13/e1/e1.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 2
+
+const unsigned int N = 3;
+
+void imprimir_matriz(double * mat, unsigned int n);
+double* crear_matriz(unsigned int n);
+//void liberar_matriz(double*mat);
 
 int main(void){
-	double ** mat;
+	double* matriz = crear_matriz(N);
+	if (matriz!=NULL){
+		imprimir_matriz(matriz, N);
+	
+		//liberar_matriz(mat);
+		free(matriz);
+		return 0;
+	}
+	else{
+	return 1;
+	}
+}
+
+void imprimir_matriz(double * mat, unsigned int n){
 	int i,j;
-	mat=(double**) calloc(N,sizeof(double*)); //reservamos un espacio para un arreglo de N punteros a double
-	if(mat==NULL){
-		printf("ERROR");
-		return 1;	
+	for(i=0; i<n; i++){
+		for(j=0;j<n;j++){
+		
+			printf("%.0f ", mat[i*n+j]);
+		
+		}
+		
+		printf("\n");
 	}
 		
-		for(i=0;i<N;i++){
-			mat[i]=(double*) calloc(N,sizeof(double));
-				
-			if(mat[i]==NULL){
-				printf("ERROR");
-				return 1;
-			}
-			
-		}
-		
-		for(i=0;i<N;i++){
-				mat[i][i]=1;
-		}
-		
-		for(i=0;i<N;i++){
-			for(j=0;j<N;j++){
-				printf("%.0f ",mat[i][j]);
-				
-			}
-			printf("\n");
-		}
-		
-		for(i=0;i<N;i++){
-			free(mat[i]);
-		}
-		free(mat);
 
-return 0;
 }
+
+double* crear_matriz(unsigned int n){
+	double *mat;
+	int i,j;
+	mat=(double*) calloc(N,sizeof(double)*N);
+	if (mat==NULL){
+		printf("ERROR");
+		return mat;
+	}
+		
+	for(i=0; i<n; i++){
+		for(j=0;j<n;j++){
+			
+			if(i==j){
+		
+				mat[i*n+j]=1;
+			}
+		
+		}
+		
+	}
+	
+	return mat;
+}
+/*
+void liberar_matriz(double* mat){
+	free(mat);
+}
+*/
